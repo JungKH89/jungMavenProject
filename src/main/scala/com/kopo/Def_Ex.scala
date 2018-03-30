f61527605fc48fbaa0bb759004416fa83aa83116
JungKH89 <kojuy2001@gmail.com>
2018-03-30 10:36:46 +0900

    IJ

--- src/main/scala/com/kopo/Def_Ex.scala
+++ src/main/scala/com/kopo/Def_Ex.scala
@@ -5,4 +5,14 @@ object Def_Ex {
     var a=Math.round(num*Math.pow(10,e))/Math.pow(10,e)
     return a
   }
+
+  //';'를 기점으로 앞, 뒤로 변수 나눠주기
+  //2017;34를 입력받으면 2017과 34로 나누기
+  def testDiv(str:String) :(Int, Int)={
+    var inputValue = str
+    var target = inputValue.split(";")
+    var yearValue = target(0)
+    var weekValue = target(1)
+    return (yearValue.toInt, weekValue.toInt)
+  }
 }
